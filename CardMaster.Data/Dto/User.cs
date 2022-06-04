35952005be08c69ce6f82752aa24ff7864b856b0
GeorgeKarlinzer <georg11.01@mail.ru>
2022-06-04 02:54:50 +0200

    Add project files.

---
+++ CardMaster.Data/Dto/User.cs
@@ -0,0 +1,15 @@
+﻿namespace CardMaster.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+     
+        public byte[] PasswordHash { get; set; }
+        
+        public DateTime Created { get; set; }
+    }
+}
